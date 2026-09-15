085afed585715e96682b94348fa8a5d976a04b36
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-09-15 10:52:59 -0300

    chore: bootstrap Spring Boot application

---
+++ src/main/java/br/com/pedropavanello/animal_adoption_api/AnimalAdoptionApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedropavanello.animal_adoption_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalAdoptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalAdoptionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedropavanello/animal_adoption_api/AnimalAdoptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedropavanello.animal_adoption_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalAdoptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
